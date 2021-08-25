95b0bfade265668142f30b21f16616698bdba902
Dieg Luna <diegofcolunalopez@gmail.com>
2021-08-25 16:26:53 -0500

    Manejo del estado

--- moonmakers-webapp-tareas/src/components/Search.js
+++ moonmakers-webapp-tareas/src/components/Search.js
@@ -1,11 +1,23 @@
 import React from "react";
-import "../css/components/Search.css"
+import "../css/components/Search.css";
 
 function Search() {
+  // para crear un estado en una funciona, con react hooks
+  const [searchValue, setSearchValue] = React.useState("");
+
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
-  }
-  return <input onChange={onSearchValueChange} className="Search" placeholder="buscar" />;
+    setSearchValue(event.target.value);
+  };
+  return [
+    <input
+      value={searchValue}
+      onChange={onSearchValueChange}
+      className="Search"
+      placeholder="buscar"
+    />,
+    <p>{searchValue}</p>
+  ];
 }
 
 export { Search };
